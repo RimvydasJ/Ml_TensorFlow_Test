ad6b4eb5285135976eb0e2e9fecdb85a8c00e3b9
kali <rimvydas.janusauskas@gmail.com>
2021-06-30 15:03:35 -0400

    SimpleRegression

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -2,11 +2,14 @@ import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
 my_feature = ([1.0, 2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0, 10.0, 11.0, 12.0])
 my_label   = ([5.0, 8.8,  9.6, 14.2, 18.8, 19.5, 21.4, 26.8, 28.9, 32.0, 33.8, 38.2])
 
-learning_rate=0.01
-epochs=10
+learning_rate=0.1
+epochs=50
 my_batch_size=12
 
 
@@ -55,9 +58,48 @@ def plot_the_loss_curve(epochs, rmse):
   plt.show()
 
 
+def predict_house_values(n, feature, label):
+  """Predict house values based on a feature."""
+
+  batch = training_df[feature][10000:10000 + n]
+  predicted_values = my_model.predict_on_batch(x=batch)
+
+  print("feature   label          predicted")
+  print("  value   value          value")
+  print("          in thousand$   in thousand$")
+  print("--------------------------------------")
+  for i in range(n):
+    print ("%5.0f %6.0f %15.0f" % (training_df[feature][10000 + i],
+                                   training_df[label][10000 + i],
+                                   predicted_values[i][0] ))
 
 my_model = build_model(learning_rate)
 trained_weight, trained_bias, epochs, rmse = train_model(my_model, my_feature, my_label, epochs, my_batch_size)
 
 plot_the_model(trained_weight, trained_bias, my_feature, my_label)
 plot_the_loss_curve(epochs, rmse)
+
+
+# Importing real data
+training_df = pd.read_csv(filepath_or_buffer="https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+training_df["median_house_value"] /= 1000.0
+training_df.head()
+
+learning_rate = 0.01
+epochs = 30
+batch_size = 30
+
+my_feature = "total_rooms" 
+my_label="median_house_value"
+
+my_model = None
+
+my_model = build_model(learning_rate)
+weight, bias, epochs, rmse = train_model(my_model, training_df, 
+                                         my_feature, my_label,
+                                         epochs, batch_size)
+                                         
+plot_the_model(weight, bias, my_feature, my_label)
+plot_the_loss_curve(epochs, rmse)
+
+
